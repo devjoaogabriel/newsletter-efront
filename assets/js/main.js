765ea922ae1fb50c6ab25e65ef3194136da618c1
devjoaogabriel <joao.gabrielmt32@gmail.com>
2022-06-15 18:56:58 -0300

    upgrade files

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/*
+function FormValidation() {
+    var name = document.forms["form"]["name"].value;
+    if (name == "") {
+        alert("Favor informar o seu nome!");
+        return false;     
+    }
+    else{
+        alert("Olá, " + name + " !");
+        return true;
+    }
+}
+*/
