a957806b3645903039207f1be6b81b95f883946c
devjoaogabriel <joao.gabrielmt32@gmail.com>
2022-09-16 12:02:44 -0300

    inclusão de arquivo js e efeito scrollreveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
-/*
-function FormValidation() {
-    var name = document.forms["form"]["name"].value;
-    if (name == "") {
-        alert("Favor informar o seu nome!");
-        return false;     
-    }
-    else{
-        alert("Olá, " + name + " !");
-        return true;
-    }
-}
-*/
+// SCROLLREVEAL ANIMATION //
+
+const ScrollAnimation = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1900,
+    delay: 200,
+    // reset: true
+})
+
+ScrollAnimation.reveal('.header')
+ScrollAnimation.reveal('.text-center, .form-class')
+ScrollAnimation.reveal('.btn_submit, .effect', {interval: 300})
+
+
+
